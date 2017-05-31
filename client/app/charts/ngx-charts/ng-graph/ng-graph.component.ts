30f7366056797369a5275cfb2d2c0a89822da845
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-05-31 15:58:08 +0200

    fix ngGraph

--- client/app/charts/ngx-charts/ng-graph/ng-graph.component.ts
+++ client/app/charts/ngx-charts/ng-graph/ng-graph.component.ts
@@ -3,6 +3,29 @@ import * as shape from 'd3-shape';
 import { colorSets } from '@swimlane/ngx-charts/release/utils/color-sets';
 import {Chart} from '../../chart.interface';
 
+const defaultOptions = {
+  view: [900, 600],
+  colorScheme: colorSets.find(s => s.name === 'cool'),
+  schemeType: 'ordinal',
+  showLegend: true,
+  legendTitle: 'Legend',
+  gradient: false,
+  showXAxis: true,
+  showYAxis: true,
+  showXAxisLabel: true,
+  showYAxisLabel: true,
+  yAxisLabel: '',
+  xAxisLabel: '',
+  autoScale: true,
+  showGridLines: true,
+  rangeFillOpacity: 0.5,
+  roundDomains: false,
+  tooltipDisabled: false,
+  showSeriesOnHover: true,
+  curve: shape.curveLinear,
+  curveClosed: shape.curveCardinalClosed
+};
+
 @Component({
   selector: 'app-ng-graph',
   templateUrl: './ng-graph.component.html',
@@ -11,35 +34,15 @@ import {Chart} from '../../chart.interface';
 })
 export class NgGraphComponent implements OnInit, Chart {
   
-  chartOptions = {
-    view: [900, 600],
-    colorScheme: colorSets.find(s => s.name === 'cool'),
-    schemeType: 'ordinal',
-    showLegend: true,
-    legendTitle: 'Legend',
-    gradient: false,
-    showXAxis: true,
-    showYAxis: true,
-    showXAxisLabel: true,
-    showYAxisLabel: true,
-    yAxisLabel: '',
-    xAxisLabel: '',
-    autoScale: true,
-    showGridLines: true,
-    rangeFillOpacity: 0.5,
-    roundDomains: false,
-    tooltipDisabled: false,
-    showSeriesOnHover: true,
-    curve: shape.curveLinear,
-    curveClosed: shape.curveCardinalClosed
-  };
-  data: any[];
-  activated: boolean = false;
+  private chartOptions: any;
+  private data: any[];
+  private activated: boolean = false;
 
 
   constructor(private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.chartOptions = {...defaultOptions};
   }
 
   setData(graphData, graphConfig) {
@@ -55,7 +58,8 @@ export class NgGraphComponent implements OnInit, Chart {
 
   load() {
     this.activated = false;
-    setTimeout(()=> { this.activated = true; this.ref.markForCheck(); }, 300);
+    this.activated = true; 
+    this.ref.markForCheck();
   }
 
   ease() {
